80778789d45d460fe28f569f1367ed793bc1fe1b
shubhra tiwary <shubhratiwary5@gmail.com>
2025-12-05 20:33:39 +0530

    token addresses added

--- scripts/fetchPrices.ts
+++ scripts/fetchPrices.ts
@@ -1,3 +1,13 @@
-async function getPriceForPool() {
+// -----------TOKEN ADDRESSES -----------
+const WETH = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
+const DAI = "0x6B175474E89094C44Da98b954EedeAC495271d0F";
+
+async function getPriceForPool(factoryAdd: string): Promise<number> {
+  console.log("starting the price fetch for the pool...");
   // this function would be a helper to fetch the reserves of the pair and calculate the price
+  return 13;
+}
+
+async function main() {
+  // here we will call the getPriceForPool function with the factory address
 }
