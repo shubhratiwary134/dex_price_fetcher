d477d1127639e1583da6af8754efbd3302d64c62
shubhra tiwary <shubhratiwary5@gmail.com>
2025-12-14 17:54:30 +0530

    parsing the CLI arguments

--- scripts/cli.ts
+++ scripts/cli.ts
@@ -1,11 +1,13 @@
-function parseArgs() {
+import { cliArgs } from "../type/cliTypes.js";
+
+function parseArgs(): cliArgs {
   const args = process.argv.slice(2);
-  const parsed: Record<string, string> = {};
+  const parsed: cliArgs = {};
 
   for (const arg of args) {
     if (!arg.includes("=")) continue;
     const [key, value] = arg.split("=");
-    parsed[key] = value;
+    parsed[key as keyof cliArgs] = value;
   }
 
   return parsed;
---
+++ type/cliTypes.ts
@@ -0,0 +1,8 @@
+export type cliArgs = {
+  mode?: string;
+  tokenIn?: string;
+  tokenOut?: string;
+  routerBuy?: string;
+  routerSell?: string;
+  tradeSize?: string;
+};
