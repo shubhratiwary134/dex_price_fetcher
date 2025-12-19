80c2829a78b32472b07126694212a7b7ab1f10f9
shubhra tiwary <shubhratiwary5@gmail.com>
2025-12-19 14:21:02 +0530

    binary search for the perfect trade size implementation

--- helpers/tradeSizeHelper.ts
+++ helpers/tradeSizeHelper.ts
@@ -1,3 +1,8 @@
+import { ethers } from "ethers";
+import { simulateTrade } from "../scripts/fetchPrices.js";
+import { getValueInUSD } from "../services/conversionServices.js";
+import { tokenDecimals } from "../services/tokenServices.js";
+
 export type OptimizationPoints = {
   size: number;
   profitTokenRaw: bigint;
@@ -16,4 +21,42 @@ async function findPerfectTradeSize(params: {
   // this function will iterate over different trade sizes to find the optimal one
   const results: OptimizationPoints[] = [];
   let bestResult: OptimizationPoints | null = null;
+
+  for (
+    let size = params.minSize;
+    size <= params.maxSize;
+    size += params.stepSize
+  ) {
+    const profitTokenRaw = await simulateTrade(
+      size,
+      params.tokenIn,
+      params.tokenOut,
+      params.routerBuy,
+      params.routerSell
+    );
+
+    const price = await getValueInUSD(params.tokenIn);
+    if (!price) continue;
+
+    const profitUSD =
+      (profitTokenRaw * price.raw) /
+      ethers.parseUnits("1", await tokenDecimals(params.tokenIn));
+
+    const point: OptimizationPoints = {
+      size,
+      profitTokenRaw: profitTokenRaw,
+      profitUSD: ethers.formatUnits(profitUSD, price.decimals),
+    };
+
+    results.push(point);
+
+    if (
+      !bestResult ||
+      BigInt(profitUSD) >
+        BigInt(ethers.parseUnits(bestResult.profitUSD, price.decimals))
+    ) {
+      bestResult = point;
+    }
+  }
+  return { results, bestResult };
 }
