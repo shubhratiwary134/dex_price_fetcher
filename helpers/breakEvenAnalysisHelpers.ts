00daba1e1e1cffbff9350b8b55e57ba63a9081fc
shubhra tiwary <shubhratiwary5@gmail.com>
2026-01-06 10:32:41 +0530

    breakEven helper created

---
+++ helpers/breakEvenAnalysisHelpers.ts
@@ -0,0 +1,10 @@
+// functions to analyze the break even point of a trade given fees and slippage
+
+/* 
+features to be added:
+
+--> Minimum viable size across all worlds 
+
+--> Viability count that feeds directly into the opportunity score
+
+*/
