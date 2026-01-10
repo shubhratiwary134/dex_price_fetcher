a2f7b8816402ae464f4e12d90713f80438cccecf
shubhra tiwary <shubhratiwary5@gmail.com>
2026-01-10 12:05:32 +0530

    viability factor introduced

--- helpers/opportunityScoreHelper.ts
+++ helpers/opportunityScoreHelper.ts
@@ -1,1 +1,13 @@
+import { worldSummary } from "../type/analysisTypes.js";
+
 // generates a score to judge a given opportunity based on various parameters
+export function generateOpportunityScore(
+  worldSummaries: worldSummary[]
+): number {
+  const total = worldSummaries.length;
+  const viable = worldSummaries.filter((w) => w.isViable).length;
+
+  const viableScore = viable / total;
+
+  return viableScore;
+}
